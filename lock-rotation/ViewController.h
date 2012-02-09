1624ac68aef6f8e0addeda6177b873d1c0a581c8
Junya Ishihara <junya@champierre.com>
2012-02-09 18:07:17 +0900

    Initial Commit

---
+++ lock-rotation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lock-rotation
+//
+//  Created by じゅんや いしはら on 12/02/09.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
